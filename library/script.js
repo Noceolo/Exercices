9b48fba1a99285af40a4653a09d50702917ffbfa
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-04-22 17:43:46 +0200

    Added library project from Odin

---
+++ library/script.js
@@ -0,0 +1,37 @@
+//         function book (title, author, pagenbr, read){
+//     if(!new.target){
+//         throw Error("You need to use the new operator to create a new object")
+//     }else{
+//         this.title = title;
+//         this.author = author;
+//         this.pagenbr = pagenbr;
+//         this.read = read;
+//         this.info = function(){
+//             return (this.title + " by " + this.author + ", " + this.pagenbr + " pages," + this.read + ".")
+//         }
+//     };
+// };
+// var lotr = new book("The Lords of the rings", "JRR Tolkien", "295", "Not read yet")
+// lotr.info();
+
+
+const myLibrary = [];
+
+function book(title, author, read){
+    if(!new.target){
+        throw Error("you need to use the new operator")
+    }else{
+        this.title = title;
+        this.author = author;
+        this.read = read;
+        this.bookID = crypto.randomUUID();
+    };
+};
+
+function addBook(title, author, read){
+    const Book = new book(title, author, read)
+
+    myLibrary.push(Book);
+    return Book;
+};
+
