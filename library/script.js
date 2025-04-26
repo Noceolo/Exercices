3234302c1a6fdc55e07580eee5156168794213fd
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-04-26 11:20:50 +0200

    Added open and close fct for popup form

--- library/script.js
+++ library/script.js
@@ -35,3 +35,11 @@ function addBook(title, author, read){
     return Book;
 };
 
+
+function openForm() {
+    document.getElementById("myForm").style.display = "flex";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
