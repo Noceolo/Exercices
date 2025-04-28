3000d890f6f48536c6e2871eeacd1cded7d36a62
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-04-28 15:45:24 +0200

    Cleaned functions to be one action only

--- library/script.js
+++ library/script.js
@@ -1,26 +1,18 @@
-//         function book (title, author, pagenbr, read){
-//     if(!new.target){
-//         throw Error("You need to use the new operator to create a new object")
-//     }else{
-//         this.title = title;
-//         this.author = author;
-//         this.pagenbr = pagenbr;
-//         this.read = read;
-//         this.info = function(){
-//             return (this.title + " by " + this.author + ", " + this.pagenbr + " pages," + this.read + ".")
-//         }
-//     };
-// };
-// var lotr = new book("The Lords of the rings", "JRR Tolkien", "295", "Not read yet")
-// lotr.info();
-
-
 const myLibrary = [];
 
 const bookContainer = document.querySelector('.bookContainer');
 const bookTitleForm = document.querySelector('.bookTitleForm');
 const bookAuthorForm = document.querySelector('.bookAuthorForm');
 const bookReadForm = document.querySelector('.bookReadForm');
+
+function openForm() {
+    document.getElementById("myForm").style.display = "flex";
+  };
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  };
+
 
 function Book(title, author, read){
     if(!new.target){
@@ -35,40 +27,10 @@ function Book(title, author, read){
 
 function addBook(title, author, read){
     const book = new Book(title, author, read)
-
     myLibrary.push(book);
 };
 
-
-function openForm() {
-    document.getElementById("myForm").style.display = "flex";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
-
-function addCardBook() {
-    const newBookTitle = bookTitleForm.value;
-    bookTitleForm.value = '';
-
-    const newBookAuthor = bookAuthorForm.value;
-    bookAuthorForm.value = '';
-
-    const newBookread = bookReadForm.value;
-    bookReadForm.value = '';
-
-    addBook(newBookTitle, newBookAuthor, newBookread)
-};
-
-function libraryDisplay() {
-    
-    const oldBookCards = document.querySelectorAll(".bookCard");
-    oldBookCards.forEach(book => book.remove());
-
-
-    myLibrary.forEach((item) => {
-        
+function createBookCard(item){
         const bookCard = document.createElement('div');
         const bookTitle = document.createElement('div');
         const bookAuthor = document.createElement('div');
@@ -85,8 +47,35 @@ function libraryDisplay() {
         bookTitle.textContent = item.title;
         bookAuthor.textContent = item.author;
         bookRead.textContent = item.read;
+};
+
+function libraryDisplay() {
+    
+    const oldBookCards = document.querySelectorAll(".bookCard");
+    oldBookCards.forEach(book => book.remove());
+    
+    
+    myLibrary.forEach((item) => {
+        createBookCard(item);
+        
     });
 };
+
+function handleFormSubmit() {
+    const newBookTitle = bookTitleForm.value;
+    bookTitleForm.value = '';
+
+    const newBookAuthor = bookAuthorForm.value;
+    bookAuthorForm.value = '';
+
+    const newBookRead = bookReadForm.value;
+    bookReadForm.value = '';
+
+    addBook(newBookTitle, newBookAuthor, newBookRead);
+    libraryDisplay();
+    closeForm();
+};
+
 
 addBook("The lord of the ring", "JRR tolkien", "Not read");
 libraryDisplay();
