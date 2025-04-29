663a06e76ceb28d87a2abdaf363616d7c912f6dd
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-04-29 17:27:32 +0200

    Created Boardgame module

---
+++ tictactoe/script.js
@@ -0,0 +1,13 @@
+const GameBoard = (function() {
+    const board = ["", "", "", "", "", "", "", "", ""];
+  
+    function getBoard() {
+      return board;
+    }
+  
+    return {
+      getBoard
+    };
+  })();
+
+
