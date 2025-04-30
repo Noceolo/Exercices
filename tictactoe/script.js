514cd2b7f7894f163563c63a34464c112fdd3f8a
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-04-30 17:45:25 +0200

    Added comments for next step

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -14,7 +14,7 @@ const GameBoard = (function() {
     };
   })();
 
-
+ //maybe have the const boardState = GameBoard.getBoard(); as a global const because used everywhere
   
   function checkFullBoard(){
     const boardState = GameBoard.getBoard();
@@ -31,7 +31,7 @@ const GameBoard = (function() {
     const boardState = GameBoard.getBoard();
     // win cons : same marker on 3 spaces in a row
     const winCons = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6], [0,4,8]];
-    // array number : 012, 345, 678, 036, 147, 258, 246, 048
+    // compare the board array with  this arrays (.this or .some)
   };
   
   
