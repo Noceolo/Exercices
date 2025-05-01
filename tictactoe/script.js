ac6a796518adb81c69d0658ec2dece8f1d3ecd16
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-05-01 17:05:23 +0200

    Nested all game logic fct inside of GameController

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -15,83 +15,92 @@ const GameBoard = (function() {
   })();
 
  //maybe have the const boardState = GameBoard.getBoard(); as a global const because used everywhere
-  
-  function checkFullBoard(){
-    const boardState = GameBoard.getBoard();
-    const fullBoard = !boardState.some(cell => cell === "");
-    
-    if (fullBoard === true){
-      return true;
-    };
-  };
-  
-  
-  
-  function checkWinCon(){
-    const boardState = GameBoard.getBoard();
-    const winCons = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6], [0,4,8]];
 
-    for (let combo of winCons){
-      
-        const first = combo[0]
-        const second = combo[1]
-        const third = combo[2]
-        if (boardState[first] === boardState[second] && boardState[second] === boardState[third] && boardState[first] !== "")
-          return true;
-      };
-    };
-  
-  
-  
-  function getPlayerChoice(){
-    let playerChoice = prompt("1 | 2 | 3 \n4 | 5 | 6\n7 | 8 | 9\n choose a space");
-    if(playerChoice === null || playerChoice === ""){
-      console.log("maybe another time then");
-    }else if(isNaN(playerChoice)){
-      console.log("You need to pick a number");
-      
-    }else if (playerChoice <= 0 || playerChoice >= 10){
-      console.log("You need to pick a space between 1 and 9 buddy");
-    }else {
-      return playerChoice = playerChoice - 1;
-    };
-  };
+ //time to put everything inside GameController
+const GameController = (function() {
+    // i'm still confused about IIFE
+    const player1 = createPlayer("Mark", "X");
+    const player2 = createPlayer("Megan", "O");
+    const getBoard = GameBoard.getBoard();
+    let activePlayer = player1;
 
-  function checkEmpty(playerChoice){
+    function playTurn(player){
+      const choice = getPlayerChoice();
+      if (choice === undefined) return;
+      if (checkEmpty(choice)){
+        placeMarker(choice,player.marker);
+      }else 
+      console.log("that space is already taken")
+      playTurn(player);
+    };
+
+    function checkEmpty(playerChoice){
       const boardState = GameBoard.getBoard();
 
       if (boardState[playerChoice] === ""){
         return true;
       }else return false;
-  };
+    };
 
-function placeMarker(playerChoice, marker){
-    const boardState = GameBoard.getBoard();
-    boardState[playerChoice] = marker
-    console.log(boardState);
+    function placeMarker(playerChoice, marker){
+      const boardState = GameBoard.getBoard();
+      boardState[playerChoice] = marker
+      console.log(boardState);
+    };
 
+    function getPlayerChoice(){
+      let playerChoice = prompt("1 | 2 | 3 \n4 | 5 | 6\n7 | 8 | 9\n choose a space");
+      if(playerChoice === null || playerChoice === ""){
+        console.log("maybe another time then");
+      }else if(isNaN(playerChoice)){
+        console.log("You need to pick a number");
+        
+      }else if (playerChoice <= 0 || playerChoice >= 10){
+        console.log("You need to pick a space between 1 and 9 buddy");
+      }else {
+        return playerChoice = playerChoice - 1;
+      };
+    };
+  
+    function switchPlayer(){
+      if (activePlayer = player1){
+        activePlayer = player2
+      }else {
+        activePlayer = player1
+      };
+    };
 
-}
-
-function playerTurn(player){
-    const choice = getPlayerChoice();
-    if (choice === undefined) return;
-    if (checkEmpty(choice)){
-      placeMarker(choice,player.marker);
-    }else 
-    console.log("that space is already taken")
-}
-
-const GameController = (function() {
-    // i'm still confused about IIFE
-    // playerTurn();
-    // checkWinCon();
-    // checkFullBoard();
-    // switch active player;
+    function checkFullBoard(){
+      const boardState = GameBoard.getBoard();
+      const fullBoard = !boardState.some(cell => cell === "");
+      
+      if (fullBoard === true){
+        return true;
+      };
+    };
+    
+    function checkWinCon(){
+      const boardState = GameBoard.getBoard();
+      const winCons = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6], [0,4,8]];
+  
+      for (let combo of winCons){
+        
+          const first = combo[0]
+          const second = combo[1]
+          const third = combo[2]
+          if (boardState[first] === boardState[second] && boardState[second] === boardState[third] && boardState[first] !== "")
+            return true;
+        };
+      };
+    function gameLoop(){
+      playTurn(activePlayer);
+      checkWinCon(); // if checkWinCon returns true, display active player as winner and stop the game loop
+      checkFullBoard(); // if checkfull board returns true, display the tie, and start a new game
+      switchPlayer();
+    };
+    gameLoop();
 })
 
 
-const player1 = createPlayer("Mark", "X");
-const player2 = createPlayer("Megan", "O");
 
 console.log("1 | 2 | 3 \n4 | 5 | 6\n7 | 8 | 9");
