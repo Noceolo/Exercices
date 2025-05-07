c9d6788bbcfa2a5b0335faba4274608f5b42edf5
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-05-07 12:07:09 +0200

    Cleaned fct, still confusing

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -40,20 +40,73 @@ const GameController = (function() {
   let activePlayer = player1;
   
   
+  
+  function resetBoard(){
+    boardState.fill("");
+  }
+  
+  
+  function resetBoardDisplay() {
+    resetBoard(); 
+    const gameCells = document.querySelectorAll('div.boardSpace');
+    gameCells.forEach(cell => {
+      cell.textContent = '';
+    });
+  }
+  function checkEmpty(playerChoice){
+    if (boardState[playerChoice] === ""){
+      return true;
+    }else return false;
+  };
+  
+  
+  function placeMarker(playerChoice, marker){
+    boardState[playerChoice] = marker
+    console.log(boardState);
+  };
+  
+  
+  function switchPlayer(){
+    if (activePlayer === player1){
+      activePlayer = player2
+    }else {
+      activePlayer = player1
+    };
+  };
+  
+  function checkFullBoard(){
+    const fullBoard = !boardState.some(cell => cell === "");
+    
+    if (fullBoard === true){
+      return true;
+    };
+  };
+  
+  function checkWinCon(){
+    const winCons = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6], [0,4,8]];
+    
+    for (let combo of winCons){
+      
+      const first = combo[0]
+      const second = combo[1]
+      const third = combo[2]
+      if (boardState[first] === boardState[second] && boardState[second] === boardState[third] && boardState[first] !== "")
+        return true;
+    };
+  };
+
   function getEventTarget(event){
     const cell = event.currentTarget
     const index = cell.dataset.index
-
-    console.log(index);
-
+  
     if (checkEmpty(index)){
       placeMarker(index, activePlayer.marker);
-
+  
       cell.textContent = activePlayer.marker;
     }else {
       alert('This space is already taken');
     };
-
+  
     if (checkWinCon()){
       activePlayer.score += 1
       if (activePlayer.score >= 3){
@@ -67,79 +120,16 @@ const GameController = (function() {
       console.log(`${player1.name} :` , player1.score ,"  vs " , `${player2.name} : `,player2.score);
       return;
     };
-
+  
     if (checkFullBoard()){
       console.log("It's a tie");
       resetBoardDisplay()
       return
     };
-
     switchPlayer();
-    
-
-  };
-
-    function resetBoard(){
-      boardState.fill("");
-   }
-    
-
-   function resetBoardDisplay() {
-    resetBoard(); // This resets your boardState array
-    
-    // Reset all cell displays
-    const gameCells = document.querySelectorAll('div.boardSpace');
-    gameCells.forEach(cell => {
-      cell.textContent = '';
-    });
-  }
-    function checkEmpty(playerChoice){
-      if (boardState[playerChoice] === ""){
-        return true;
-      }else return false;
-    };
-    
-    
-    function placeMarker(playerChoice, marker){
-      boardState[playerChoice] = marker
-      console.log(boardState);
-    };
   
-    
-    function switchPlayer(){
-      if (activePlayer === player1){
-        activePlayer = player2
-      }else {
-        activePlayer = player1
-      };
-    };
-    
-    function checkFullBoard(){
-      const fullBoard = !boardState.some(cell => cell === "");
-      
-      if (fullBoard === true){
-        return true;
-      };
-    };
-    
-    function checkWinCon(){
-      const winCons = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6], [0,4,8]];
-      
-      for (let combo of winCons){
-        
-        const first = combo[0]
-        const second = combo[1]
-        const third = combo[2]
-        if (boardState[first] === boardState[second] && boardState[second] === boardState[third] && boardState[first] !== "")
-          return true;
-      };
-    };
-
-    function StartGame(){
-      console.log("1 | 2 | 3 \n4 | 5 | 6\n7 | 8 | 9");
-      playTurn();
-    }
-    return { StartGame };
+  };
+  
 })();
 
 
